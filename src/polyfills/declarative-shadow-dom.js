962d1cbf49f4c35476da51984c2e3e721ea316b1
RogierdeRuijter <rogierderuijter@hotmail.com>
2023-12-01 18:16:01 +0100

    Revert "Feature/webcomponet"

--- src/polyfills/declarative-shadow-dom.js
+++
@@ -1,9 +0,0 @@
-export function attachShadowRoots(root) {
-  root.querySelectorAll("template[shadowrootmode]").forEach((template) => {
-    const mode = template.getAttribute("shadowrootmode");
-    const shadowRoot = template.parentNode.attachShadow({ mode });
-    shadowRoot.appendChild(template.content);
-    template.remove();
-    attachShadowRoots(shadowRoot);
-  });
-}
