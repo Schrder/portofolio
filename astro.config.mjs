25d94a4dacd28572c2aa4b9512aafc013150e1da
Rogier <r.de.ruijter@vpro.nl>
2024-03-18 21:36:00 +0100

    use scopedStyle class

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tomvanderbijll.com",
-  scopedStyleStrategy: "where",
+  scopedStyleStrategy: "class",
   build: {
     target: "safari11"
   }
